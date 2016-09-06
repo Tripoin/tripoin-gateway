75dddbb29d1b9de913529de435bfbee5418309ad
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2016-09-06 07:53:40 +0700

    refactor group id and package

--- tripoin-root-util/tripoin-util-ui/src/main/java/com/tripoin/util/ui/geo/shared/GeolocatorClientRpc.java
+++
@@ -1,9 +0,0 @@
-package com.tripoin.util.ui.geo.shared;
-
-import com.vaadin.shared.communication.ClientRpc;
-
-public interface GeolocatorClientRpc extends ClientRpc {
-
-    public void detectCurrentPosition(int callbackid);
-
-}
