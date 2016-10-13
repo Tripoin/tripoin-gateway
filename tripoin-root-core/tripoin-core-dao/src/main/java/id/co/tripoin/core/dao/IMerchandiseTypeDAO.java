9c2373a754ede0ef7c7f6da921938640fe16661d
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2016-10-13 14:17:49 +0700

    Merchandise Type DAO

---
+++ tripoin-root-core/tripoin-core-dao/src/main/java/id/co/tripoin/core/dao/IMerchandiseTypeDAO.java
@@ -0,0 +1,12 @@
+package id.co.tripoin.core.dao;
+
+
+import id.co.tripoin.core.pojo.pos.MerchandiseType;
+
+/**
+ * 
+ * @author <a href="mailto:fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
+ *
+ */
+public interface IMerchandiseTypeDAO extends IScaffoldingDAO<MerchandiseType>{
+}
